7d61a74d80997ae22f3d36b3fd50fba6ba9500c2
David Wadden <dwadden@pivotal.io>
2019-02-09 11:03:36 -0600

    Support basic authentication over ES6 REST client
      + Support multiple ES nodes with differing auth
      + Uses credentials within URL https://user:pass@host/path

--- es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/ElasticSearchRestClientBuilderProvider.java
+++ es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/ElasticSearchRestClientBuilderProvider.java
@@ -1,6 +1,11 @@
 package com.netflix.conductor.elasticsearch;
 
+import java.util.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 
@@ -9,8 +14,14 @@ import javax.inject.Provider;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ElasticSearchRestClientBuilderProvider implements Provider<RestClientBuilder> {
+
+    private static final Logger logger =
+        LoggerFactory.getLogger(ElasticSearchRestClientBuilderProvider.class);
+
     private final ElasticSearchConfiguration configuration;
 
     @Inject
@@ -18,9 +29,24 @@ public class ElasticSearchRestClientBuilderProvider implements Provider<RestClie
         this.configuration = configuration;
     }
 
+    @SuppressWarnings("Duplicates")
     @Override
     public RestClientBuilder get() {
-        return RestClient.builder(convertToHttpHosts(configuration.getURIs()));
+        RestClientBuilder builder = RestClient.builder(convertToHttpHosts(configuration.getURIs()));
+
+        // assumes credentials for all endpoints are same as userInfo from first URI in list
+        URI endpointUri = configuration.getURIs().get(0);
+        if (StringUtils.isNotEmpty(endpointUri.getUserInfo())) {
+            String encodedUserInfo =
+                Base64.getEncoder().encodeToString(endpointUri.getUserInfo().getBytes());
+            String authHeaderValue = "Basic " + encodedUserInfo;
+            Header authHeader = new BasicHeader(HttpHeaders.AUTHORIZATION, authHeaderValue);
+
+            builder.setDefaultHeaders(new Header[]{authHeader});
+            logger.info("Setting Elasticsearch Authorization header: {}", authHeaderValue);
+        }
+
+        return builder;
     }
 
     private HttpHost[] convertToHttpHosts(List<URI> hosts) {
--- es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/ElasticSearchRestClientProvider.java
+++ es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/ElasticSearchRestClientProvider.java
@@ -1,15 +1,33 @@
 package com.netflix.conductor.elasticsearch;
 
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ElasticSearchRestClientProvider implements Provider<RestClient> {
+
+    private static final Logger logger =
+        LoggerFactory.getLogger(ElasticSearchRestClientProvider.class);
 
     private final ElasticSearchConfiguration configuration;
 
@@ -18,9 +36,19 @@ public class ElasticSearchRestClientProvider implements Provider<RestClient> {
         this.configuration = configuration;
     }
 
+    @SuppressWarnings("Duplicates")
     @Override
     public RestClient get() {
-        return RestClient.builder(convertToHttpHosts(configuration.getURIs())).build();
+        RestClientBuilder builder = RestClient.builder(convertToHttpHosts(configuration.getURIs()));
+
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        convertToAuthScopeCredentials(configuration.getURIs())
+            .forEach(credentialsProvider::setCredentials);
+
+        builder.setHttpClientConfigCallback(
+            httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+        return builder.build();
     }
 
     private HttpHost[] convertToHttpHosts(List<URI> hosts) {
@@ -29,6 +57,31 @@ public class ElasticSearchRestClientProvider implements Provider<RestClient> {
                 .collect(Collectors.toList());
 
         return list.toArray(new HttpHost[list.size()]);
+    }
+
+    private Map<AuthScope, Credentials> convertToAuthScopeCredentials(List<URI> hosts) {
+        // filter hosts to those with user info credentials
+        List<URI> userInfoHosts = hosts.stream()
+            .filter(host -> StringUtils.isNotEmpty(host.getUserInfo()))
+            .collect(Collectors.toList());
+
+        // build map keyed on hosts to credentials
+        return userInfoHosts.stream()
+            .collect(Collectors.toMap(
+                host -> new AuthScope(host.getHost(), host.getPort()),
+                host -> {
+                    String usernamePassword =
+                        Arrays.toString(Base64.getDecoder().decode(host.getUserInfo()));
+                    final int atColon = usernamePassword.indexOf(':');
+                    if (atColon >= 0) {
+                        return new UsernamePasswordCredentials(
+                            usernamePassword.substring(0, atColon),
+                            usernamePassword.substring(atColon + 1));
+                    } else {
+                        return new UsernamePasswordCredentials(usernamePassword, null);
+                    }
+                }
+            ));
     }
 
 }
